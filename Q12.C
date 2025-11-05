9d3682f42076b29ba3e1d47833959fecce61598b
theofficialneelesh <neelesh25csu161@ncuindia.edu>
2025-11-05 15:55:24 +0530

    Create Q12.C

---
+++ Q12.C
@@ -0,0 +1,23 @@
+int main() {
+    int arr[100], n, i, max, min;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d numbers: ", n);
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    max = min = arr[0];
+
+    for(i = 1; i < n; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+        if(arr[i] < min)
+            min = arr[i];
+    }
+
+    printf("Maximum = %d\nMinimum = %d\n", max, min);
+
+    return 0;
+}
